b17232f618eb7bf70d87bfd2723bbbbeda396e56
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-05-16 21:30:41 +0800

    bullet effect learn script 0516
    0516

---
+++ Assets/Scrips/LearnData.cs
@@ -0,0 +1,23 @@
+// 單行註解
+/* 多行用
+ * 註解
+ */
+// 藍色 保留字
+// 白色 名稱  變數
+// 綠色 資料類型
+using UnityEngine;      //使用UNITY引擎
+
+// 修飾詞  類別(藍圖)   類別名稱  繼承(物件導向程式設計 OOP 三大原則)
+public class LearnData : MonoBehaviour
+{
+    // LearnData類別成員
+    //儲存資料方式
+    //欄位 feild
+    //資料類型 類型名稱 結束符號;   int 整數 float 浮點數 string 字串 bool 布林值 
+    int PlayerHp;        //血量
+    float PlayerExp;    //經驗
+    string PlayerName;  //名字
+    bool PlayerHuman;   //判斷種族
+
+}
+// 非LearnData類別

f4805d58fb7ad03590d90b56d9915bb17151fc17
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-06-01 20:56:44 +0800

    method

--- Assets/Scrpts/LearnMethod.cs
+++ Assets/Scrpts/LearnMethod.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 public class LearnMethod : MonoBehaviour
 {
     //方法語法
-   
-
-    void Test()
+    int ichi = 1;
+    int ni = 2;
+    private void Start()
     {
-        int ichi = 1;
-        int ni = 2;
+
+        ColorText();
+        print("1+2=多少");
+        if (ni > ichi)
+        {
+            Test();
+        }
+        else
+        {
+            print("錯");
+        }
+    }
+
+    private void Test()
+    {
+        //int ichi = 3; //測試method內重新帶入變數
         print(ichi + ni);
+    }
+    private void ColorText()
+    {
+        print("<color=yellow>我是黃色</color>");
+        print("<color=#1f1e33>歌名</color>");
     }
 }

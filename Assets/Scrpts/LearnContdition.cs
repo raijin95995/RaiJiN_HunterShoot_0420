2a18672a4a61ff6cab44fb3c6af62618b97df360
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-06-27 21:23:32 +0800

    C#

--- Assets/Scrpts/LearnContdition.cs
+++ Assets/Scrpts/LearnContdition.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class LearnContdition : MonoBehaviour
 {
-    public bool death = true ;
-
+    public bool death = true;
+    public int combo = 0;
     private void Start()
     {
         if (true)
@@ -18,14 +18,33 @@ public class LearnContdition : MonoBehaviour
 
     private void Update()
     {
-        if (death)
+        //if (death)
+        //{
+        //    print("你已經死了");
+        //}
+        //else
+        // {
+        //    print("你已經活了");
+        // }
+        if (combo < 100)
         {
-            print("你已經死了");
+            print("NO");
         }
         else
+
+            if (combo >= 200)
         {
-            print("你已經活了");
+            print("GOOD");
         }
+        else
+                if (combo >= 100)
+        {
+            print("GREAT");
+        }
+
+
+
+
     }
 
 }

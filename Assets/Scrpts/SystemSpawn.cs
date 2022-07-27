115935c259d3241eb3533f10e565495f4d3b3f70
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-07-27 20:31:02 +0800

    隨機

--- Assets/Scrpts/SystemSpawn.cs
+++ Assets/Scrpts/SystemSpawn.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RAIJIN
 {
@@ -14,15 +16,43 @@ namespace RAIJIN
         [Header("怪物陣列"),SerializeField]
         private GameObject[] monsters;
 
+        [Header("生成位置 第二排"), SerializeField]
+        private Transform[] traSecondPlace ;
+
+        [SerializeField]
+        private List<Transform> ListSecondPlace = new List<Transform>();
+
+
         #endregion
 
         #region 事件
-        
+
+        private void Awake()
+        {
+            SpawnRandom();
+        }
+
         #endregion
-        
-        
-        
+
+
+
         #region 方法
+
+
+        void SpawnRandom()
+        {
+            int min = 2;  //最小值
+            int max = traSecondPlace.Length;  //抓取陣列長度
+
+            print(Random.Range(min, max));   //不會抓到最大值
+
+            //ListSecondPlace = traSecondPlace.ToList();
+
+            System.Random random = new System.Random();
+
+            ListSecondPlace = ListSecondPlace.OrderBy(x => random.Next()).ToList();
+
+        }
 
         #endregion
 

1c159dfe413a15c81172678b61ac31fda8e5eecd
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-08-08 20:03:07 +0800

    血量

---
+++ Assets/Scrpts/SystemHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace RAIJIN
+{
+
+    /// <summary>
+    /// 控制血量
+    /// </summary>
+    /// 
+    public class SystemHealth : MonoBehaviour
+    {
+
+        [Header("傷害物件"), SerializeField]
+        private GameObject goDamage;
+        [Header("血量圖片"), SerializeField]
+        private Image imgHp;
+        [Header("血量文字"), SerializeField]
+        private TextMeshProUGUI textHp;
+
+
+    }
+}
+

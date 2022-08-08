9c8ee7e5f96871fc7ef34922e7f6737c582cb9b3
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-08-08 20:45:58 +0800

    怪物資料

---
+++ Assets/Scrpts/DataEnemy.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+namespace RAIJIN
+{
+    /// <summary>
+    /// 腳本化物件
+    /// </summary>
+    [CreateAssetMenu (menuName ="RAIJIN/Data Enemy" , fileName = "Data Enemy") ]
+    public class DataEnemy : ScriptableObject
+    {
+
+    }
+}
+
--- Assets/Scrpts/SystemHealth.cs
+++ Assets/Scrpts/SystemHealth.cs
@@ -8,17 +8,22 @@ namespace RAIJIN
     /// <summary>
     /// 控制血量
     /// </summary>
-    /// 
     public class SystemHealth : MonoBehaviour
     {
 
-        [Header("傷害物件"), SerializeField]
+        [Header("畫布傷害物件"), SerializeField]
         private GameObject goDamage;
         [Header("血量圖片"), SerializeField]
         private Image imgHp;
         [Header("血量文字"), SerializeField]
         private TextMeshProUGUI textHp;
 
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+
+        }
 
     }
 }

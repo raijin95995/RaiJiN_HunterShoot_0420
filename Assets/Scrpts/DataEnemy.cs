6b5a97ed919b014499b78b29be3badd6230ad01e
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-08-08 21:30:38 +0800

    你已經死了

--- Assets/Scrpts/DataEnemy.cs
+++ Assets/Scrpts/DataEnemy.cs
@@ -6,9 +6,13 @@ namespace RAIJIN
     /// <summary>
     /// 腳本化物件
     /// </summary>
-    [CreateAssetMenu (menuName ="RAIJIN/Data Enemy" , fileName = "Data Enemy") ]
+    [CreateAssetMenu(menuName = "RAIJIN/Data Enemy", fileName = "Data Enemy")]
     public class DataEnemy : ScriptableObject
     {
+        [Header("血量"), Range(0, 10000)]
+        public float hp;
+        [Header("傷害"), Range(0, 1000)]
+        public float damage;
 
     }
 }
--- Assets/Scrpts/SystemHealth.cs
+++ Assets/Scrpts/SystemHealth.cs
@@ -17,11 +17,38 @@ namespace RAIJIN
         private Image imgHp;
         [Header("血量文字"), SerializeField]
         private TextMeshProUGUI textHp;
+        [Header("怪物資料"), SerializeField]
+        private DataEnemy dataEnemy;
 
+        private float hp;
+
+        private void Awake()
+        {
+            hp = dataEnemy.hp;
+            textHp.text = hp.ToString();
+        }
 
         private void OnCollisionEnter(Collision collision)
         {
-            
+
+            GetHurt();
+        }
+
+        void GetHurt()
+        {
+            hp -= 100;
+
+            textHp.text = hp.ToString();
+
+            imgHp.fillAmount = hp / dataEnemy.hp;
+
+            if (hp <= 0) Die();
+
+        }
+
+        void Die()
+        {
+            print("你已經死了");
 
         }
 

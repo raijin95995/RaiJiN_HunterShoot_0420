ea5bb6b2a697ac3221968e4ff3757e3bdc9b77a7
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-09-05 19:15:19 +0800

    計數子彈

--- Assets/Scrpts/SystemEatBullet.cs
+++ Assets/Scrpts/SystemEatBullet.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 
 namespace RAIJIN
 {
+
+	/// <summary>
+	/// 吃子彈系統
+	/// </summary>
 	public class SystemEatBullet : MonoBehaviour
 	{
 		private string nameBullet = "子彈";
+		private SystemTurn systemTurn;
+
+
+		private void Awake()
+		{
+
+			systemTurn = FindObjectOfType<SystemTurn>();
+
+		}
+
 
 
 		private void OnTriggerEnter(Collider other)
 		{
+
+
 			if (other.name.Contains(nameBullet))
 			{
+				systemTurn.EatBullet();
 				Destroy(gameObject);
 
 			}
--- Assets/Scrpts/SystemTurn.cs
+++ Assets/Scrpts/SystemTurn.cs
@@ -3,76 +3,82 @@ using UnityEngine.Events;
 
 namespace RAIJIN
 {
-    /// <summary>
-    /// ╰参
-    /// </summary>
-    public class SystemTurn : MonoBehaviour
-    {
-        private SystemControl systemControl;
-        private SystemSpawn systemSpawn;
-        private RecycleArea recycleArea;
+	/// <summary>
+	/// ╰参
+	/// </summary>
+	public class SystemTurn : MonoBehaviour
+	{
+		private SystemControl systemControl;
+		private SystemSpawn systemSpawn;
+		private RecycleArea recycleArea;
 
-        private int totalCountBullets;  //祇甮紆羆计
-        private int totalCountEnemysLive; //ぶン
+		private int totalCountBullets;  //祇甮紆羆计
+		private int totalCountEnemysLive; //ぶン
 
-        private int totalRecycleBullets;
+		private int totalRecycleBullets;
 
-        public UnityEvent onTurnEnemy;
+		public UnityEvent onTurnEnemy;
 
-        private bool canSpawn = true ;  //ネ┣
+		private bool canSpawn = true;  //ネ┣
 
-
-        private void Awake()
-        {
-            systemControl = GameObject.Find("").GetComponent<SystemControl>();
-            systemSpawn = GameObject.Find("ネ┣╰参").GetComponent<SystemSpawn>();
-            recycleArea = GameObject.Find("Μ跋办").GetComponent<RecycleArea>();
-
-            recycleArea.onRecyele.AddListener(RecycleBullet);
-        }
+		private int countEatBullet;
 
 
-        private void RecycleBullet()
-        {
-            totalCountBullets = systemControl.howManyCanShootBullet;
+		private void Awake()
+		{
+			systemControl = GameObject.Find("").GetComponent<SystemControl>();
+			systemSpawn = GameObject.Find("ネ┣╰参").GetComponent<SystemSpawn>();
+			recycleArea = GameObject.Find("Μ跋办").GetComponent<RecycleArea>();
 
-            totalRecycleBullets++;
-            print("紆ㄓ:"+ totalRecycleBullets);
-
-            if(totalRecycleBullets == totalCountBullets)
-            {
-                print("turn over");
-                onTurnEnemy.Invoke();
-            }
-        }
-
-        public void MoveEndSpawnEnemy()
-        {
-            if (!canSpawn) return;
-            canSpawn = false;
-            systemSpawn.SpawnRandom();
-            Invoke("PlayerTurn", 1f);
-            
+			recycleArea.onRecyele.AddListener(RecycleBullet);
+		}
 
 
-        }
+		private void RecycleBullet()
+		{
+			totalCountBullets = systemControl.howManyCanShootBullet;
 
-        public void PlayerTurn()
-        {
-            systemControl.canShootBullet = true;
-            canSpawn = true;
-            totalRecycleBullets = 0;
-        }
+			totalRecycleBullets++;
+			print("紆ㄓ:" + totalRecycleBullets);
+
+			if (totalRecycleBullets == totalCountBullets)
+			{
+				print("turn over");
+				onTurnEnemy.Invoke();
+			}
+		}
+
+		public void MoveEndSpawnEnemy()
+		{
+			if (!canSpawn) return;
+			canSpawn = false;
+			systemSpawn.SpawnRandom();
+			Invoke("PlayerTurn", 1f);
 
 
 
+		}
+
+		public void PlayerTurn()
+		{
+			systemControl.canShootBullet = true;
+			canSpawn = true;
+			totalRecycleBullets = 0;
+		}
+
+
+
+		public void EatBullet()
+		{
+			countEatBullet++;
+		}
 
 
 
 
 
 
-    }
+	}
 
 }
 

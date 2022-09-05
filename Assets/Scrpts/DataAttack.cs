22aec1179a255b6b63cf49c3be9e6af8681019b9
raijin95995 <103936818+raijin95995@users.noreply.github.com>
2022-09-05 21:04:19 +0800

    腳本化物件 攻擊力

---
+++ Assets/Scrpts/DataAttack.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace RAIJIN
+{
+    /// <summary>
+    /// 腳本化物件
+    /// </summary>
+    [CreateAssetMenu(menuName = "RAIJIN/Data Attack", fileName = "Data Attack")]
+    public class DataAttack : ScriptableObject
+    {
+
+        [Header("攻擊力"), Range(0, 10000)]
+        public float attack;
+        [Header("攻擊力浮動"), Range(0, 100)]
+        public float attackFloat;
+
+    }
+}
+
